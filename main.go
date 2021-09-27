665a72b121f3445bc364f1a58db865b48d803eda
Brian Starkey <stark3y@gmail.com>
2021-09-27 21:55:04 +0100

    Implement READ and read boot2

--- main.go
+++ main.go
@@ -4,18 +4,21 @@ package main
 
 import (
 	"bytes"
-	"context"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"os"
-	"time"
 
 	tty "github.com/jacobsa/go-serial/serial"
 )
 
 var (
-	OpcodeSync   [4]byte = [4]byte{ 'P', 'I', 'C', 'O' }
-	ResponseSync [4]byte = [4]byte{ 'S', 'Y', 'N', 'C' }
+	OpcodeSync   [4]byte = [4]byte{ 'S', 'Y', 'N', 'C' }
+	OpcodeRead   [4]byte = [4]byte{ 'R', 'E', 'A', 'D' }
+	ResponseSync [4]byte = [4]byte{ 'P', 'I', 'C', 'O' }
+	ResponseOK   [4]byte = [4]byte{ 'O', 'K', 'O', 'K' }
+	ResponseErr  [4]byte = [4]byte{ 'E', 'R', 'R', '!' }
 )
 
 type SyncCommand struct {
@@ -46,6 +49,45 @@ func (c *SyncCommand) Execute(rw io.ReadWriter) error {
 	return nil
 }
 
+type ReadCommand struct {
+	Addr uint32
+	Len  uint32
+	Data []byte
+}
+
+func (c *ReadCommand) Execute(rw io.ReadWriter) error {
+	// Re-use for command and response.
+	buf := make([]byte, len(OpcodeRead) + 4 + 4, len(OpcodeRead) + int(c.Len))
+
+	copy(buf[0:], OpcodeRead[:])
+	binary.LittleEndian.PutUint32(buf[4:], c.Addr)
+	binary.LittleEndian.PutUint32(buf[8:], c.Len)
+
+	n, err := rw.Write(buf)
+	if err != nil {
+		return err
+	} else if n != len(OpcodeRead) + 4 + 4 {
+		return fmt.Errorf("unexpectead write length: %v", n)
+	}
+
+	// Re-slice to full size
+	buf = buf[:cap(buf)]
+
+	n, err = rw.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	if !bytes.HasPrefix(buf, ResponseOK[:]) {
+		return fmt.Errorf("received error response")
+	}
+
+	// Slice off the response
+	c.Data = buf[len(ResponseOK):]
+
+	return nil
+}
+
 func run() error {
 	options := tty.OpenOptions{
 		PortName: "/dev/ttyUSB0",
@@ -62,28 +104,35 @@ func run() error {
 	}
 	defer port.Close()
 
-	// TODO: Save Cancel somewhere
-	ctx, _ := context.WithCancel(context.Background())
-
-	tick := time.NewTicker(300 * time.Millisecond)
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case <- tick.C:
-			port.Write([]byte("foolio"))
-		default:
-			var sc SyncCommand
-			err = (&sc).Execute(port)
-			if err != nil {
-				fmt.Println("sync:", err)
-			} else {
-				fmt.Println("Synced!")
-			}
-			time.Sleep(1 * time.Second)
+	// Try and sync
+	for i := 0; i < 5; i++ {
+		var sc SyncCommand
+		err = (&sc).Execute(port)
+		if err != nil {
+			fmt.Println("sync:", err)
+		} else {
+			fmt.Println("Synced!")
+			break
 		}
 	}
+
+	// Bail out
+	if err != nil {
+		return err
+	}
+
+	rc := &ReadCommand{
+		Addr: 0x10000000,
+		Len:  256,
+	}
+
+	err = rc.Execute(port)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("0x%8x, %d bytes\n", rc.Addr, rc.Len)
+	fmt.Println(hex.Dump(rc.Data))
 
 	return nil
 }
